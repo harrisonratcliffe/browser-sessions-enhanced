dfd47b09fd7cdc43dbe8f9960a36d596eb5f0c87
Harrison Ratcliffe <harrison.ratcliffe@me.com>
2024-11-09 17:12:18 +0000

    Update BrowserSessionsEnhanced.php

--- src/Facades/BrowserSessionsEnhanced.php
+++ src/Facades/BrowserSessionsEnhanced.php
@@ -4,6 +4,11 @@ namespace Harrisonratcliffe\BrowserSessionsEnhanced\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \Illuminate\Support\Collection sessions()
+ * @method static void logoutOtherBrowserSessions()
+ * @method static \Illuminate\Support\Carbon|string getUserLastActivity(bool $human = false)
+ */
 class BrowserSessionsEnhanced extends Facade
 {
     protected static function getFacadeAccessor(): string
